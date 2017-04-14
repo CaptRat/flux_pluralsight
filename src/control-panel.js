3162888a6563186f8fa0af3d5809a48ac56d13e1
Patrick Logan <JPatrickLogan@gmail.com>
2017-04-13 22:34:10 -0600

    feat (dispatch) Build code-along Flux app

--- src/control-panel.js
+++ src/control-panel.js
@@ -1,78 +1,19 @@
-import { Dispatcher, Store } from './flux';
+import { Dispatcher} from './flux';
 
 const controlPanelDispatcher = new Dispatcher();
 
-export const UPDATE_USERNAME = `UPDATE_USERNAME`;
-export const UPDATE_FONT_SIZE_PREFERENCE = `UPDATE_FONT_SIZE_PREFERENCE`;
+document.getElementById('userNameInput').addEventListener('input', ({target}) =>{
+  const name = target.value;
+  console.log("Dispatching...", name);
+  controlPanelDispatcher.dispatch('TODO__NAMEINPUTACTION');
+})
 
-const userNameUpdateAction = (name)=>{
-    return {
-        type: UPDATE_USERNAME,
-        value: name
-    }
-}
+document.forms.fontSizeForm.fontSize.forEach(element=>{
+  element.addEventListener('change',({target})=>{
+    controlPanelDispatcher.dispatch('TODO__FONTUPDATEACTION');
+  })
+})
 
-const fontSizePreferenceUpdateAction = (size)=>{
-    return {
-        type: UPDATE_FONT_SIZE_PREFERENCE,
-        value: size
-    }
-}
-//
-class UserPrefsStore extends Store {
-    getInitialState() {
-        return localStorage[`preferences`] ? JSON.parse(localStorage[`preferences`]) : {
-            userName: "Jim",
-            fontSize: "small"
-        };
-    }
-    __onDispatch(action){
-        switch(action.type) {
-            case UPDATE_USERNAME:
-                this.__state.userName = action.value;
-                this.__emitChange();
-                break;
-            case UPDATE_FONT_SIZE_PREFERENCE:
-                this.__state.fontSize = action.value;
-                this.__emitChange();
-                break;
-        }
-    }
-    getUserPreferences(){
-        return this.__state;
-    }
-}
-
-const userPrefsStore = new UserPrefsStore(controlPanelDispatcher);
-const userNameInput = document.getElementById(`userNameInput`);
-userNameInput.addEventListener("input",({target})=>{
-    const name = target.value;
-    controlPanelDispatcher.dispatch(userNameUpdateAction(name));
-});
-
-const fontSizeForm = document.forms.fontSizeForm;
-
-fontSizeForm.fontSize.forEach(element=>{
-    element.addEventListener("change",({target})=>{
-        console.log("Buton change...",target.value);
-        controlPanelDispatcher.dispatch(fontSizePreferenceUpdateAction(target.value));
-    })
-});
-
-const render = ({userName,fontSize})=>{
-    document.getElementById("userName").innerText = userName;
-    document.getElementsByClassName("container")[0].style.fontSize = fontSize === "small" ? "16px" : "24px";
-    fontSizeForm.fontSize.value = fontSize;
-
-}
-
-userPrefsStore.addListener((state)=>{
-    render(state);
-    saveUserPreferences(state);
-});
-
-const saveUserPreferences =(state)=>{
-    localStorage[`preferences`] = JSON.stringify(state);
-}
-
-render(userPrefsStore.getUserPreferences());
+controlPanelDispatcher.register(action=>{
+  console.log("Recieved action...", action);
+})
